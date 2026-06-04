8031dd4bbcb41c7c5cbb3f41a4bd39ec26468ae1
DmitriiDudarow <dudarow.d@yandex.ru>
2026-06-04 18:02:18 +0500

    фиксанул какашку

--- orders_service/main.py
+++ orders_service/main.py
@@ -32,7 +32,7 @@ app = FastAPI(
 
 Instrumentator().instrument(app).expose(app)
 
-app.include_router(orders_router, prefix="/api")
+app.include_router(orders_router, prefix="/api/v1")
 
 if __name__ == "__main__":
     uvicorn.run("orders_service.main:app", host="0.0.0.0", port=8002, reload=True)
